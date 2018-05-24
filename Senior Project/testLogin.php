74fff67d5e6bda51173aae389ecdc6cd46a91f46
Braden <bradensaito@gmail.com>
2018-05-23 20:49:41 -0700

    main folder is now SeniorProject (no spaces)

---
+++ Senior Project/testLogin.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+define('DB_NAME', 'chowder_test1');
+define('DB_USER', 'chowder_test1@localhost');
+define('DB_PASSWORD', 'capstone');
+define('DB_HOST', 'db4free.net:3306');
+
+HI?
+
+$link = mysql_connect(DB_HOST, DB_NAME, DB_PASSWORD);
+
+if(!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+echo 'Hey there'
+
+?>
---
+++ SeniorProject/testLogin.php
@@ -0,0 +1,20 @@
+﻿<html>
+<body>
+	<?php
+
+	define('DB_NAME', 'chowder_test1');
+	define('DB_USER', 'chowder_test1@localhost');
+	define('DB_PASSWORD', 'capstone');
+	define('DB_HOST', 'db4free.net:3306');
+
+	$link = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
+
+	if(!$link) {
+	    die('Could not connect: ' . mysql_error());
+	}
+
+	echo 'Successfully connected to database';
+
+	?>
+</body>
+</html>
